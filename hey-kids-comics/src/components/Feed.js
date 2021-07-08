3d2d5037216678077968b7af5ee8ea239da824f1
Jason Wesson <jsnwesson@gmail.com>
2021-07-08 16:27:54 -0500

    Make it possible to click on series image to open modal

--- hey-kids-comics/src/components/Feed.js
+++ hey-kids-comics/src/components/Feed.js
@@ -5,10 +5,37 @@ const Feed = ({addToList, currentSeriesQuery, currentIssuesQuery, searchIssues})
   const [open, setOpen] = React.useState(false)
 
     const feed = currentSeriesQuery.map((series) => {
+      console.log(series.title)
       return (
         <div key={series.id}>
-            <Segment>
-              <Image onClick={() => {searchIssues(series.id)}} src={series.thumbnail} />
+          <Segment>
+            <Modal
+              key={series.id}
+              open={open}
+              onClose={() => setOpen(false)}
+              onOpen={() => setOpen(true)}
+              size='tiny'
+              trigger={
+                <Image onClick={() => {searchIssues(series.id)}} src={series.thumbnail} />}
+            >
+              {currentIssuesQuery.map((issue) => {
+                return (
+                  <Modal.Content key={issue.id}>
+                    <Card centered color='red' raised >
+                      <Image
+                        label={issue.digitalId > 0 ? { as: 'a', color: 'red', corner: 'right', icon: 'desktop' } : null}
+                        src={issue.thumbnail}
+                        wrapped
+                      />
+                      <Card.Content textAlign='center'>
+                        <Card.Header>{issue.title}</Card.Header>
+                      </Card.Content>
+                    </Card>
+                  </Modal.Content>
+                  )
+              })}
+            </Modal>
+
             <Modal
               key={series.id}
               open={open}
@@ -22,7 +49,6 @@ const Feed = ({addToList, currentSeriesQuery, currentIssuesQuery, searchIssues})
                   }}
                 >{series.title}</Button>}
             >
-              <Modal.Header>{series.title}</Modal.Header>
               {currentIssuesQuery.map((issue) => {
                 return (
                   <Modal.Content key={issue.id}>
